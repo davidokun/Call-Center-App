ad6dcd04e0a1d9393d785f31ef588e11a66a724e
davidokun <davidokun99@gmail.com>
2018-01-14 17:41:19 -0500

    Implementation in the Controller class to receive calls from users.

--- src/main/java/com/almundo/callcenter/controller/CallCenterController.java
+++ src/main/java/com/almundo/callcenter/controller/CallCenterController.java
@@ -2,22 +2,49 @@ package com.almundo.callcenter.controller;
 
 import com.almundo.callcenter.model.Call;
 import com.almundo.callcenter.service.Dispatcher;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Random;
+
 @RestController("v1")
 public class CallCenterController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CallCenterController.class);
 
     @Autowired
     private Dispatcher dispatcher;
 
+
     @PostMapping("call")
     public void receiveCall(@RequestBody Call call) {
 
+
+        call.setId(new Random().nextInt(100));
+        LOGGER.info("Call No: " + call.getId() + " Received in Call Center");
+        sendCallToDispatcher(call);
     }
 
+    @PostMapping("bulk/call")
+    public void receiveCalls(@RequestBody List<Call> calls) {
+
+        calls.forEach(call -> {
+            call.setId(new Random().nextInt(100));
+            LOGGER.info("Call No: " + call.getId() + " Received in Call Center");
+            sendCallToDispatcher(call);
+        });
+    }
+
+
+    /**
+     * Send calls to CallDispatcher
+     * @param call a call received in the Call Center
+     */
     private void sendCallToDispatcher(Call call) {
 
         dispatcher.receiveCall(call);
